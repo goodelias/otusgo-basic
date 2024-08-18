ded3d89ee756b1654bcb0f9db6b0629e538a8717
goodelias <iliya.chepkin@gmail.com>
2024-08-18 15:04:39 +0100

    hw2 complete

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,29 +1,37 @@
-package init
+package main
 
 import (
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
-func init() {
-	var path string = "data.json"
+var ErrBadInput = errors.New("unexpected newline")
+
+func main() {
+	path := "data.json"
 
 	fmt.Printf("Enter data file path: ")
-	fmt.Scanln(&path)
+
+	if _, err := fmt.Scanln(&path); err != nil {
+		if err.Error() != "unexpected newline" {
+			fmt.Println("Error reading input:", err)
+			return
+		}
+	}
 
 	var err error
 	var staff []types.Employee
 
-	if len(path) == 0 {
-		path = "data.json"
-	} else {
-	}
-
 	staff, err = reader.ReadJSON(path, -1)
-
-	fmt.Print(err)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	printer.PrintStaff(staff)
 }
