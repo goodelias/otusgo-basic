ca52a20ef319ce3451fca013b0b4b00067209d33
goodelias <iliya.chepkin@gmail.com>
2024-08-27 14:36:31 +0100

    Refactor code
    Implemented chessboard generation and display functions. Fixed error handling in chessboard size input

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -6,11 +6,26 @@ func main() {
 	var size1 int
 	var size2 int
 
-	fmt.Print("Enter size 1 for chessboard\n")
-	fmt.Scanf("%d", &size1)
-	fmt.Print("Enter size 2 for chessboard\n")
-	fmt.Scanf("%d", &size2)
+	size1 = getSize("Enter size1 for chessboard from 1 to 100: ")
+	size2 = getSize("Enter size2 for chessboard from 1 to 100: ")
 
+	displayBoard(size1, size2)
+}
+
+func getSize(prompt string) int {
+	var size int
+	fmt.Print(prompt)
+	for {
+		_, err := fmt.Scanf("%d", &size)
+		if err == nil && size >= 1 && size <= 100 {
+			break
+		}
+		fmt.Println("Неверный ввод, попробуйте еще раз.")
+	}
+	return size
+}
+
+func displayBoard(size1, size2 int) {
 	for i := 0; i < size1; i++ {
 		for j := 0; j < size2; j++ {
 			if (i+j)%2 == 0 {
