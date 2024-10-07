24ea01efb773f03d26751502af3004db140b264d
goodelias <iliya.chepkin@gmail.com>
2024-10-07 15:01:17 +0100

    fix: Update regex to improve word matching
    Modified the regex to accurately capture words with digits in the words

--- hw07_word_counter/component/counter.go
+++ hw07_word_counter/component/counter.go
@@ -9,7 +9,7 @@ func CountWords(text string) map[string]int {
 	wordCountMap := make(map[string]int)
 
 	toLowerText := strings.ToLower(text)
-	re := regexp.MustCompile(`\p{L}+'?\p{L}*`)
+	re := regexp.MustCompile(`\p{L}+'?\p{L}*\d*\p{L}*|\d+\p{L}*`)
 	words := re.FindAllString(toLowerText, -1)
 
 	for _, word := range words {
