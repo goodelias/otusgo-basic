b02af384bde78f05359e4e2f84fcf1a20cbdca20
goodelias <iliya.chepkin@gmail.com>
2024-10-09 12:54:09 +0100

    perf: move regex compilation to package level to avoid recompilation in each function call

--- hw07_word_counter/component/counter.go
+++ hw07_word_counter/component/counter.go
@@ -5,11 +5,13 @@ import (
 	"strings"
 )
 
+var re = regexp.MustCompile(`\p{L}+'?\p{L}*\d*\p{L}*|\d+\p{L}*`)
+
 func CountWords(text string) map[string]int {
 	wordCountMap := make(map[string]int)
 
 	toLowerText := strings.ToLower(text)
-	re := regexp.MustCompile(`\p{L}+'?\p{L}*\d*\p{L}*|\d+\p{L}*`)
+
 	words := re.FindAllString(toLowerText, -1)
 
 	for _, word := range words {
