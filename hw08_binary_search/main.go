386d1b0f600462b44a14a13b13d6cc56e4b1b2c7
goodelias <iliya.chepkin@gmail.com>
2024-10-11 13:26:35 +0100

    refactor: Change binarySearch to return int only

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func binarySearch(arr []int, target int) (int, bool) {
+func binarySearch(arr []int, target int) int {
 	left, right := 0, len(arr)-1
 
 	for left <= right {
@@ -12,21 +12,21 @@ func binarySearch(arr []int, target int) (int, bool) {
 
 		switch {
 		case arr[mid] == target:
-			return mid, true
+			return mid
 		case arr[mid] < target:
 			left = mid + 1
 		default:
 			right = mid - 1
 		}
 	}
-	return -1, false
+	return -1
 }
 
 func main() {
 	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	target := 9
-	index, found := binarySearch(s, target)
-	if !found {
+	target := 10
+	index := binarySearch(s, target)
+	if index == -1 {
 		fmt.Printf("Element %d not found", target)
 		return
 	}
--- hw08_binary_search/main_test.go
+++ hw08_binary_search/main_test.go
@@ -12,60 +12,47 @@ func TestBinarySearch(t *testing.T) {
 		arr    []int
 		target int
 		want   int
-		found  bool
 	}{
 		{
 			name:   "with existing target",
 			arr:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
 			target: 3,
 			want:   2,
-			found:  true,
 		},
 		{
 			name:   "with not existing target",
 			arr:    []int{1, 2, 3, 4, 5},
 			target: 6,
 			want:   -1,
-			found:  false,
 		},
 		{
 			name:   "with empty array",
 			arr:    []int{},
 			target: 1,
 			want:   -1,
-			found:  false,
 		},
 		{
 			name:   "with first target element",
 			arr:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
 			target: 1,
 			want:   0,
-			found:  true,
 		},
 		{
 			name:   "with last target element",
 			arr:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
 			target: 9,
 			want:   8,
-			found:  true,
 		},
 		{
 			name:   "with single target element",
 			arr:    []int{5},
 			target: 5,
 			want:   0,
-			found:  true,
 		},
 	}
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			got, found := binarySearch(test.arr, test.target)
-			if test.found != true {
-				assert.False(t, found)
-				assert.Equal(t, -1, got)
-				return
-			}
-			assert.True(t, found)
+			got := binarySearch(test.arr, test.target)
 			assert.Equal(t, test.want, got)
 		})
 	}
