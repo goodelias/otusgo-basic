368324502e6be2560aa5e54ea1da565ddb5f9a7a
goodelias <iliya.chepkin@gmail.com>
2024-10-11 12:57:13 +0100

    refactor: Replace error with boolean return in binarySearch

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-var ErrElementNotFound = errors.New("element not found")
-
-func binarySearch(arr []int, target int) (int, error) {
+func binarySearch(arr []int, target int) (int, bool) {
 	left, right := 0, len(arr)-1
 
 	for left <= right {
@@ -15,23 +12,23 @@ func binarySearch(arr []int, target int) (int, error) {
 
 		switch {
 		case arr[mid] == target:
-			return mid, nil
+			return mid, true
 		case arr[mid] < target:
 			left = mid + 1
 		default:
 			right = mid - 1
 		}
 	}
-	return -1, ErrElementNotFound
+	return -1, false
 }
 
 func main() {
 	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	target := 10
-	search, err := binarySearch(s, target)
-	if err != nil {
-		fmt.Println(err)
+	target := 9
+	index, found := binarySearch(s, target)
+	if !found {
+		fmt.Printf("Element %d not found", target)
 		return
 	}
-	fmt.Printf("Element %d found at index %d\n", target, search)
+	fmt.Printf("Element %d found at index %d\n", target, index)
 }
--- hw08_binary_search/main_test.go
+++ hw08_binary_search/main_test.go
@@ -8,64 +8,64 @@ import (
 
 func TestBinarySearch(t *testing.T) {
 	testCases := []struct {
-		name    string
-		arr     []int
-		target  int
-		want    int
-		wantErr error
+		name   string
+		arr    []int
+		target int
+		want   int
+		found  bool
 	}{
 		{
-			name:    "with existing target",
-			arr:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
-			target:  3,
-			want:    2,
-			wantErr: nil,
+			name:   "with existing target",
+			arr:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			target: 3,
+			want:   2,
+			found:  true,
 		},
 		{
-			name:    "with not existing target",
-			arr:     []int{1, 2, 3, 4, 5},
-			target:  6,
-			want:    -1,
-			wantErr: ErrElementNotFound,
+			name:   "with not existing target",
+			arr:    []int{1, 2, 3, 4, 5},
+			target: 6,
+			want:   -1,
+			found:  false,
 		},
 		{
-			name:    "with empty array",
-			arr:     []int{},
-			target:  1,
-			want:    -1,
-			wantErr: ErrElementNotFound,
+			name:   "with empty array",
+			arr:    []int{},
+			target: 1,
+			want:   -1,
+			found:  false,
 		},
 		{
-			name:    "with first target element",
-			arr:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
-			target:  1,
-			want:    0,
-			wantErr: nil,
+			name:   "with first target element",
+			arr:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			target: 1,
+			want:   0,
+			found:  true,
 		},
 		{
-			name:    "with last target element",
-			arr:     []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
-			target:  9,
-			want:    8,
-			wantErr: nil,
+			name:   "with last target element",
+			arr:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			target: 9,
+			want:   8,
+			found:  true,
 		},
 		{
-			name:    "with single target element",
-			arr:     []int{5},
-			target:  5,
-			want:    0,
-			wantErr: nil,
+			name:   "with single target element",
+			arr:    []int{5},
+			target: 5,
+			want:   0,
+			found:  true,
 		},
 	}
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			got, err := binarySearch(test.arr, test.target)
-			if test.wantErr != nil {
-				assert.Error(t, err)
+			got, found := binarySearch(test.arr, test.target)
+			if test.found != true {
+				assert.False(t, found)
 				assert.Equal(t, -1, got)
 				return
 			}
-			assert.NoError(t, err)
+			assert.True(t, found)
 			assert.Equal(t, test.want, got)
 		})
 	}
