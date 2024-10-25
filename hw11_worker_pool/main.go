891ef5d5e2efdc509e9c8370319d49ab88f52c4a
goodelias <iliya.chepkin@gmail.com>
2024-10-25 21:45:02 +0100

    hw11 is completed

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func Inc(id int, c *Counter) {
+	c.mu.Lock()
+	c.count++
+	fmt.Printf("Worker %d incremented the count to %d\n", id, c.count)
+	c.mu.Unlock()
+}
+
+func worker(id int, c *Counter) {
+	Inc(id, c)
+}
+
+func startWorkers(numWorkers int, c *Counter, wg *sync.WaitGroup) error {
+	if numWorkers < 0 {
+		return fmt.Errorf("number of workers cannot be negative: %d", numWorkers)
+	}
+	for i := 1; i <= numWorkers; i++ {
+		wg.Add(1)
+		go func(id int) {
+			worker(id, c)
+			wg.Done()
+		}(i)
+	}
+	return nil
+}
+
 func main() {
-	// Place your code here.
+	counter := &Counter{}
+	wg := sync.WaitGroup{}
+
+	err := startWorkers(-2, counter, &wg)
+	if err != nil {
+		fmt.Printf("Failed to start worker: %v", err)
+		return
+	}
+
+	wg.Wait()
 }
